98ab4a00cd38617cc121eb379cd1818d9c17b41a
Kakimori Takao <okakikakikaki@gmail.com>
2023-03-12 14:45:08 +0000

    remove head.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,39 @@
 import "../../styles/globals.css";
+import { Inter } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: "next-template",
+  description: "Next.js template",
+  generator: "Next.js",
+  applicationName: "Next.js",
+  referrer: "strict-origin-when-cross-origin",
+  keywords: ["Next.js", "React", "JavaScript", "TypeScript"],
+  authors: [{ name: "Urotea" }],
+  colorScheme: "light",
+  creator: "Urotea",
+  publisher: "",
+  alternates: {},
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {},
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -6,8 +41,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html data-theme="light" lang="ja">
-      <head />
+    <html data-theme="light" lang="ja" className={inter.className}>
       <body>{children}</body>
     </html>
   );
