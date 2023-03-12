33413e5cb441114a256df6d1d28e721e70582a02
Kakimori Takao <okakikakikaki@gmail.com>
2023-03-12 09:07:45 +0000

    update apollo

--- src/pages/api/v1/graphql.ts
+++ src/pages/api/v1/graphql.ts
@@ -1,5 +1,3 @@
-import { ApolloServer, Config } from "apollo-server-micro";
-import { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
 import { Resolvers } from "@/generated/graphql";
 import queryResolver from "@/graphql/server/resolvers/queryResolver";
@@ -9,10 +7,10 @@ import teamResolver from "@/graphql/server/resolvers/teamResolver";
 import todoResolver from "@/graphql/server/resolvers/todoResolver";
 import userResolver from "@/graphql/server/resolvers/userResolver";
 import UserRepository from "@/graphql/server/repositories/UserRepository";
-import { Context } from "@/graphql/server/context";
 import TodoRepository from "@/graphql/server/repositories/TodoRepository";
 import TeamRepository from "@/graphql/server/repositories/TeamRepository";
-import { API_URL } from "@/constant";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer, ApolloServerOptions, BaseContext } from "@apollo/server";
 
 const resolvers: Resolvers = {
   Query: queryResolver,
@@ -23,37 +21,29 @@ const resolvers: Resolvers = {
   User: userResolver,
 };
 
-const serverConfig: Config = {
+interface ContextType extends BaseContext {
+  userRepository: UserRepository;
+  todoRepository: TodoRepository;
+  teamRepository: TeamRepository;
+}
+
+const serverConfig: ApolloServerOptions<ContextType> = {
   typeDefs: [
     fs.readFileSync(`${process.cwd()}/graphql/v1/schema/query.graphql`, {
       encoding: "utf8",
     }),
   ],
   resolvers: resolvers,
-  context: ({ req }): Context => {
+};
+
+const apolloServer = new ApolloServer(serverConfig);
+
+export default startServerAndCreateNextHandler<ContextType>(apolloServer, {
+  context: async (req, res) => {
     const userRepository = new UserRepository();
     const todoRepository = new TodoRepository();
     const teamRepository = new TeamRepository();
 
-    return { userRepository, todoRepository, teamRepository };
+    return { req, res, userRepository, todoRepository, teamRepository };
   },
-};
-
-const apolloServer = new ApolloServer(serverConfig);
-const startServer = apolloServer.start();
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  await startServer;
-  await apolloServer.createHandler({
-    path: API_URL,
-  })(req, res);
-}
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+});
